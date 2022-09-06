7b87437b4b31f36628ca88889a2b80b739667af6
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-09-06 15:54:42 -0400

    Updated JS
    *SERVER.JS*
    
    - Updated Server.js
    
    *Schema.SQL*
    
    - Added Schema.sql to remove Database if it exists and  create Database afterwards
    
    *CONNECTION.JS*
    
    - Started building connection.js

---
+++ config/connection.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+let sequelize;
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const session = require("express-session");
+// const session = require("express-session");
+const sequelize = require("./config/connection");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // GET route
-app.get("/", async (req, res) =>{
-    res.send("test");
-})
+app.get("/", async (req, res) => {
+  res.send("test");
+});
 
-
-
-app.listen(PORT, ()=>{
-    console.log("Server is up and running at localhost:" + PORT)
-})
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running at localhost:" + PORT);
+  });
+});
