4064e5fbf205cd0e5b72c61361c5e4244ecc3aee
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-09-06 15:40:31 -0400

    Updated JS
    *SERVER.JS*
    
    - Added middleware and a get route
    - Added express-session
    
    *USER.JS*
    
    - Created User Model

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const session = require("express-session");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// GET route
+app.get("/", async (req, res) =>{
+    res.send("test");
+})
+
+
 
 app.listen(PORT, ()=>{
     console.log("Server is up and running at localhost:" + PORT)
