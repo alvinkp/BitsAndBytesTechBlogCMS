793236257dd805cb4ece88d85acfb326702bd75b
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-09-07 17:38:25 -0400

    Updated User.Js
    *USER.JS*
    
    - Added post route for user creation

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,8 +1,37 @@
+const { User } = require("../../models");
+
 const router = require("express").Router();
 
+// GET api/user route
 router.get("/", async (req, res) => {
   console.log("You're in the user route");
   res.end();
+});
+
+// Create New User route
+router.post("/", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if ((!username, !email, !password)) {
+    return res.status(400).json({
+      message:
+        "You did not submit all required information: Username, Email Address & Password",
+    });
+  }
+
+  try {
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+    });
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong while trying to create your user.",
+    });
+  }
 });
 
 module.exports = router;
