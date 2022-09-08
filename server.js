c116f58e3c3b8bfc6e835a67a79c2c740782e5cc
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-09-08 08:53:18 -0400

    Updated JS
    Started integrating Handlebars

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
+const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const { sequelize, sessionConfig } = require("./config/connection");
+const exphbs = require("express-handlebars");
 const controllers = require("./controllers");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const hbs = exphbs.create();
 
 // Middleware
 app.use(session(sessionConfig));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(controllers);
 
