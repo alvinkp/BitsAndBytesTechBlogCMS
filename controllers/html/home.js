2fce8f7653fc2d3bc2a57b493bf898e93cf6eb1c
alvinkp <102767990+alvinkp@users.noreply.github.com>
2022-09-10 07:33:04 -0400

    Updated JS
    *HTML/HOME.JS*
    
    - Added /login route
    
    *LOGIN.JS*
    
    - Added code for Login.js
    
    *MAIN.HANDLEBARS*
    
    - Added bootstrap elements
    
    - Setup conditional for displaying / hiding login/ logout button
    
    *LOGIN.JS*
    
    - Added code for login.js

--- controllers/html/home.js
+++ controllers/html/home.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.render("home");
+  const isLoggedIn = req.session.isLoggedIn;
+  res.render("home", {
+    isLoggedIn,
+  });
+});
+
+router.get("/login", async (req, res) => {
+  const isLoggedIn = req.session.isLoggedIn;
+  res.render("login", {
+    isLoggedIn,
+  });
 });
 
 module.exports = router;
